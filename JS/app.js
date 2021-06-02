1989436a8a76e5de5d3619bd5035d856cfabe258
yazeed alshami <alshamiyazeed@gmail.com>
2021-06-03 00:48:45 +0300

    do fram

--- JS/app.js
+++ JS/app.js
@@ -35,18 +35,21 @@ function Location(locName,minCustmer,maxCustmer,avgcookie){
         for(let i=0;i<time.length-2 ;i++){
             this.custmerNum.push(randomValue(this.minCustmer,this.maxCustmer));
             
-        }
         
     };
-    
+}
     
     Location.prototype.culcCookies= function(){
         for(let i=0;i<time.length-2;i++){
             this.cookiesNum.push(Math.floor(this.custmerNum[i] *this.avgcookie));
             this.total = this.total + this.cookiesNum[i];
-        };
+        }
+        console.log(this.custmerNum);
         console.log(this.cookiesNum);
         console.log(this.avgcookie);
+        console.log(this.minCustmer);
+        console.log(this.maxCustmer);
+
     };
     Location.prototype.tablehead=function(){
         for(let i=0;i<time.length;i++){
@@ -54,7 +57,7 @@ function Location(locName,minCustmer,maxCustmer,avgcookie){
             firstRow.appendChild(th);
 
             th.textContent = time[i];
-        };
+        }
     };
     Location.prototype.render=function(){
         let secondRow = document.createElement('tr');
@@ -65,12 +68,33 @@ function Location(locName,minCustmer,maxCustmer,avgcookie){
                 td = document.createElement('td');
                 secondRow.appendChild(td);
                 td.textContent =  this.cookiesNum[i];
-            };
+            }
             td = document.createElement('td');
                 secondRow.appendChild(td);
                 td.textContent =  this.total;
 
         };
+        Location.prototype.lastrow =function(){
+            let secondRow = document.createElement('tr');
+                table.appendChild(secondRow);
+                secondRow.textContent = 'Total by Hourly';
+                let allLocatinTotal=0;
+                for(let i=0;i<this.cookiesNum.length;i++){
+                    let sum = 0;
+                    for(let j=0;j<arrObj.length;j++) {
+                      sum = sum + arrObj[j].cookiesNum[i];
+                    }
+                     allLocatinTotal += sum; 
+                    td = document.createElement('td');
+                secondRow.appendChild(td);
+                td.textContent =  sum;
+                    
+                }
+                td = document.createElement('td');
+                secondRow.appendChild(td);
+                td.textContent =  allLocatinTotal;
+
+        }
         
 
     
@@ -96,196 +120,22 @@ function Location(locName,minCustmer,maxCustmer,avgcookie){
     dubai.render();
     paris.render();
    lima.render();
-    
-       
-        
+   seattle.lastrow();
 
-/*
-         let ulsim = document.createElement('ul');
-         let h2Tag = document.getElementById('Seattle');
-         let lisim = document.createElement('li');
-         ulsim.appendChild(lisim);
-         ulsim.textContent = `Seattle`;
-        h2Tag.appendChild(ulsim);
-    for(let i=0 ; i<14;i++){
-         lisim = document.createElement('li');
-        ulsim.appendChild(lisim);
-        let numcookies = Math.floor(this.randomNum(this.min,this.max) * this.avgcookie);
-        this.arr.push(numcookies);
-        lisim.textContent = `${time[i]} ${this.arr[i]} cookies`;
+    const shopForm = document.getElementById('locationForm');
+    shopForm.addEventListener('submit',handleSubmitting);
+    function handleSubmitting(event){
+        event.preventDefault();
+        let locationName= event.target.nameField.value;
+        let minCust= Number(event.target.minField.value);
+        let maxCust= Number(event.target.maxField.value);
+        let avgcookie= Number(event.target.avgField.value);
 
-        console.log(time[5]);
-        this.Total = this.Total + this.arr[i];
-    }
-    
-    lisim.textContent = `Total : ${this.Total} cookies`;
-    console.log(sumAnyArray(this.arr))
-    },
-    
-};
-Seattle.simulated();
-
-let Tokyo ={
-    min:3,
-    max:24,
-    avgcookie: 1.2,
-    randomNum: function(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-
-    },
-    arr:[],
-    simulated:function(){
-         let ulsim = document.createElement('ul');
-         let h2Tag = document.getElementById('Tokyo');
-         let lisim = document.createElement('li');
-         ulsim.appendChild(lisim);
-         ulsim.textContent = `Tokyo`;
-        h2Tag.appendChild(ulsim);
-    for(let i=0 ; i<14;i++){
-         lisim = document.createElement('li');
-        ulsim.appendChild(lisim);
-        let numcookies = Math.floor(this.randomNum(this.min,this.max) * this.avgcookie);
-        let result= this.arr.push(numcookies);
-        lisim.textContent = `${time[i]} ${this.arr[i]} cookies`;
-        console.log(time[5]);
-    }
-    console.log(this.arr);
-    function sum(a, b) {
-        let array = [a+b]
-        return a+b;
-        }
-        function sumAnyArray(Array) {
-            let array = [ Array.reduce(sum)];
-            return array;
-          
-          }
-    lisim.textContent = `Total : ${sumAnyArray(this.arr)} cookies`;
-    console.log(sumAnyArray(this.arr))
-    },
-    
-};
-Tokyo.simulated();
-
-let Dubai ={
-    min:11,
-    max:38,
-    avgcookie: 3.7,
-    randomNum: function(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-
-    },
-    arr:[],
-    simulated:function(){
-         let ulsim = document.createElement('ul');
-         let h2Tag = document.getElementById('Dubai');
-         let lisim = document.createElement('li');
-         ulsim.appendChild(lisim);
-         ulsim.textContent = `Dubai`;
-        h2Tag.appendChild(ulsim);
-    for(let i=0 ; i<14;i++){
-         lisim = document.createElement('li');
-        ulsim.appendChild(lisim);
-        let numcookies = Math.floor(this.randomNum(this.min,this.max) * this.avgcookie);
-        let result= this.arr.push(numcookies);
-        lisim.textContent = `${time[i]} ${this.arr[i]} cookies`;
-        console.log(time[5]);
-    }
-    console.log(this.arr);
-    function sum(a, b) {
-        let array = [a+b]
-        return a+b;
-        }
-        function sumAnyArray(Array) {
-            let array = [ Array.reduce(sum)];
-            return array;
-          
-          }
-    lisim.textContent = `Total : ${sumAnyArray(this.arr)} cookies`;
-    console.log(sumAnyArray(this.arr))
-    },
-    
-};
-Dubai.simulated();
-
-let Paris ={
-    min:20,
-    max:38,
-    avgcookie: 2.3,
-    randomNum: function(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-
-    },
-    arr:[],
-    simulated:function(){
-         let ulsim = document.createElement('ul');
-         let h2Tag = document.getElementById('Paris');
-         let lisim = document.createElement('li');
-         ulsim.appendChild(lisim);
-         ulsim.textContent = `Paris`;
-        h2Tag.appendChild(ulsim);
-    for(let i=0 ; i<14;i++){
-         lisim = document.createElement('li');
-        ulsim.appendChild(lisim);
-        let numcookies = Math.floor(this.randomNum(this.min,this.max) * this.avgcookie);
-        let result= this.arr.push(numcookies);
-        lisim.textContent = `${time[i]} ${this.arr[i]} cookies`;
-        console.log(time[5]);
-    }
-    console.log(this.arr);
-    function sum(a, b) {
-        let array = [a+b]
-        return a+b;
-        }
-        function sumAnyArray(Array) {
-            let array = [ Array.reduce(sum)];
-            return array;
-          
-          }
-    lisim.textContent = `Total : ${sumAnyArray(this.arr)} cookies`;
-    console.log(sumAnyArray(this.arr))
-    },
-    
-};
-Paris.simulated();
-
-let Lima ={
-    min:2,
-    max:16,
-    avgcookie: 4.6,
-    randomNum: function(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-
-    },
-    arr:[],
-    simulated:function(){
-         let ulsim = document.createElement('ul');
-         let h2Tag = document.getElementById('Lima');
-         let lisim = document.createElement('li');
-         ulsim.appendChild(lisim);
-         ulsim.textContent = `Lima`;
-        h2Tag.appendChild(ulsim);
-    for(let i=0 ; i<14;i++){
-         lisim = document.createElement('li');
-        ulsim.appendChild(lisim);
-        let numcookies = Math.floor(this.randomNum(this.min,this.max) * this.avgcookie);
-        let result= this.arr.push(numcookies);
-        lisim.textContent = `${time[i]} ${this.arr[i]} cookies`;
-        console.log(time[5]);
-    }
-    console.log(this.arr);
-    function sum(a, b) {
-        let array = [a+b]
-        return a+b;
-        }
-        function sumAnyArray(Array) {
-            let array = [ Array.reduce(sum)];
-            return array;
-          
-          }
-    lisim.textContent = `Total : ${sumAnyArray(this.arr)} cookies`;
-    console.log(sumAnyArray(this.arr))
-    },
-    
-};
-Lima.simulated();
- */
+        let location = new Location(locationName,minCust,maxCust,avgcookie);
+        table.deleteRow(arrObj.length);
+        console.log(arrObj.length)
+        location.randomCustmer();
+        location.culcCookies();
+        location.render();
+        seattle.lastrow();
+    };
